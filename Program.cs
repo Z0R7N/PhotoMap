e46057a0f1387f2dd56ced91f4e7bea0aa4c06ad
Z0R7N <46117076+Z0R7N@users.noreply.github.com>
2020-10-29 06:01:23 +0300

    добавление в реестр контестного меню

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace PhotoMap
 {
@@ -16,6 +18,22 @@ namespace PhotoMap
             else
             {
                 checkArgs(args);
+            testFileRec(args);
+            }
+        }
+
+        //запись аргументов в файл на рабочем столе
+        private static void testFileRec(string[] args)
+        {
+            string filePath = @"D:\rabstol\logger.txt";
+            string s = String.Join(" ", args);  
+            
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                    sw.Write(s);
+                foreach (var item in args)
+                {
+                }
             }
         }
 
@@ -23,18 +41,26 @@ namespace PhotoMap
         static void contextMap()
         {
             const string appName = "PhotoMap";
-            const string regKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
-            using (RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(regKey, true))
+            string pathApp = string.Format("\"{0}\"", System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string contextMenu = @"AllFilesystemObjects\shell\PhotoMap";
+            pathApp = pathApp.Substring(1, pathApp.Length - 2);
+            string command = @"AllFilesystemObjects\shell\PhotoMap\command";
+            using (RegistryKey contextReg = Registry.ClassesRoot.CreateSubKey(contextMenu, true))
             {
-                registryKey.SetValue(appName, string.Format("\"{0}\"", System.Reflection.Assembly.GetExecutingAssembly().Location));
+                contextReg.SetValue("MUIVerb", "Посмотреть на карте");
+                contextReg.SetValue("icon", pathApp);
+                contextReg.SetValue
             }
-
+            //using (RegistryKey comReg = Registry.ClassesRoot.CreateSubKey(command, true))
+            //{
+            //    comReg.SetValue("(По умолчанию)", "\"" + pathApp + "\" %1");
+            //}
         }
 
         // обработка аргументов
         static void checkArgs(string[] coords)
         {
-            Debug.WriteLine(coords[0]);
+
         }
     }
 }
